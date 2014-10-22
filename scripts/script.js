c2de85cc26991d459bf04044fe16d57783ef74f5
gwake1 <gwake1@gmail.com>
2014-10-22 11:10:51 -0500

    Addition of Buy.html and content

--- scripts/script.js
+++ scripts/script.js
@@ -3,21 +3,49 @@
 		var signup = document.querySelector('#signup');
 		var signin = document.querySelector('#signin');
 		var cancel = document.querySelector('.cancel');
-		
+		var loginModalToggleAreas = document.querySelectorAll('.lightbox, .login, .cancel');
+
+		/*for (var i=0; i<loginModalToggleAreas.length; i++){
+			loginModalToggleAreas[i].addEventListener('click', function(){
+			toggle('.loginmodal');
+			});
+		}
+		*/	
 		lightbox.addEventListener('click', function(){
-		hide(document.querySelector('.loginmodal'));});
+		toggle('.loginmodal');});
 		
 		signup.addEventListener('click', function(){
-		show(document.querySelector('.loginmodal'));});
+		toggle(document.querySelector('.loginmodal'));});
 		
 		signin.addEventListener('click', function(){
-		show(document.querySelector('.loginmodal'));});
+		toggle(document.querySelector('.loginmodal'));});
 
 		cancel.addEventListener('click', function(){
-		hide(document.querySelector('.loginmodal'));});
-		});
+		toggle(document.querySelector('.loginmodal'));});
+				
 
+		function toggle(element){
+		if (element.classList)
+		  element.classList.toggle('hidden')
+		}
+		else {
+		var classes = element.className.split(' ');
+		var classIndex;
+		for (var i=0; i< classes.length; i++;) {
+			if (classes[i] === 'hidden')
+			existingIndex =i;
+		}
+		
+		if (classIndex)
+			classes.splice(classIndex,1);
+		else
+			classes.psh('hidden');
+		element.className = classes.join(' ');
+		
+		}
+		}
 
+		/*
 		function hide(element){
 		if (element.classlist)
 			element.classList.add('hidden');
@@ -31,6 +59,5 @@
 		else
 			element.className = element.className.replace(new RegExp('(^|\\b)' + "hidden".split(' ').join('|') + '(\\b|$)', 'gi'), ' ');			
 		}
-
-
-
+		*/
+	
