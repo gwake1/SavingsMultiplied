cf87503cab64fd03a6cb2b1542e67e6c4f6f31e2
gwake1 <gwake1@gmail.com>
2014-10-22 12:58:59 -0500

    Refactor Login Modal and lightbox

--- scripts/script.js
+++ scripts/script.js
@@ -1,63 +1,26 @@
-	document.addEventListener('DOMContentLoaded', function(){
-		var lightbox = document.querySelector('.lightbox');
-		var signup = document.querySelector('#signup');
-		var signin = document.querySelector('#signin');
-		var cancel = document.querySelector('.cancel');
-		var loginModalToggleAreas = document.querySelectorAll('.lightbox, .login, .cancel');
+document.addEventListener('DOMContentLoaded', function(){
+  var loginModal = document.querySelector('.loginmodal');
+  var lightbox = document.querySelector('.lightbox');
+  var signIn = document.getElementById('signIn');
+  var cancelButton = document.getElementById('cancelButton');
 
-		/*for (var i=0; i<loginModalToggleAreas.length; i++){
-			loginModalToggleAreas[i].addEventListener('click', function(){
-			toggle('.loginmodal');
-			});
-		}
-		*/	
-		lightbox.addEventListener('click', function(){
-		toggle('.loginmodal');});
-		
-		signup.addEventListener('click', function(){
-		toggle(document.querySelector('.loginmodal'));});
-		
-		signin.addEventListener('click', function(){
-		toggle(document.querySelector('.loginmodal'));});
+  function hide(element){
+    element.style.display="none";
+  }
 
-		cancel.addEventListener('click', function(){
-		toggle(document.querySelector('.loginmodal'));});
-				
+  function show(element){
+    element.style.display="block";
+  }
 
-		function toggle(element){
-		if (element.classList)
-		  element.classList.toggle('hidden')
-		}
-		else {
-		var classes = element.className.split(' ');
-		var classIndex;
-		for (var i=0; i< classes.length; i++;) {
-			if (classes[i] === 'hidden')
-			existingIndex =i;
-		}
-		
-		if (classIndex)
-			classes.splice(classIndex,1);
-		else
-			classes.psh('hidden');
-		element.className = classes.join(' ');
-		
-		}
-		}
+  lightbox.addEventListener('click', function(){
+   hide(loginModal);
+   });
 
-		/*
-		function hide(element){
-		if (element.classlist)
-			element.classList.add('hidden');
-		else
-			element.className += " " + "hidden";
-		}
+   signIn.addEventListener('click', function(){
+   show(loginModal);
+   });
 
-		function show(element){
-		if (element.classlist)
-			element.classList.remove('hidden');
-		else
-			element.className = element.className.replace(new RegExp('(^|\\b)' + "hidden".split(' ').join('|') + '(\\b|$)', 'gi'), ' ');			
-		}
-		*/
-	
+   cancelButton.addEventListener('click', function(){
+   hide(loginModal);
+   });
+});
